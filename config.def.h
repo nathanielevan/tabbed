d934a579d30c44d53fca8660c66b4d2b6bda9389
Nathaniel Evan <nathanielevan@zohomail.com>
2023-02-04 18:07:57 +0700

    Change font comments

--- config.def.h
+++ config.def.h
@@ -1,12 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-// static const char font[]        = "FantasqueSansMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+// static const char font[]        = "FantasqueSansMono Nerd Font:pixelsize=13:antialias=true:autohint=true";
 // static const char font[]        = "FuraMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
 // static const char font[]        = "InconsolataLGC Nerd Font:pixelsize=12:antialias=true:autohint=true";
 // static const char font[]        = "Input Nerd Font:pixelsize=12:antialias=true:autohint=true";
 static const char font[]        = "Iosevka Nerd Font:pixelsize=13:antialias=true:autohint=true";
 // static const char font[]        = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+// static const char font[]        = "JuliaMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
 // static const char font[]        = "MesloLGM Nerd Font:pixelsize=12:antialias=true:autohint=true";
 // static const char font[]        = "mononoki Nerd Font:pixelsize=13:antialias=true:autohint=true";
 // static const char font[]        = "VictorMono Nerd Font:pixelsize=13:antialias=true:autohint=true";
