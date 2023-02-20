06502772fa73d964b3e4c9671e85ff26f7dca87f
Nathaniel Evan <nathanielevan@zohomail.com>
2023-02-21 06:16:22 +0700

    Change to default keybinds

--- config.def.h
+++ config.def.h
@@ -37,23 +37,23 @@ static Bool npisrelative  = True;
         .v = (char *[]){ "/bin/sh", "-c", \
                 "prop=\"`xwininfo -children -id $1 | grep '^     0x' |" \
                 "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
-                "xargs -0 printf %b | dmenu -l 10 -h 24 -fn \"$2\" -w $1`\" &&" \
+                "xargs -0 printf %b | dmenu -l 10 -h 21 -fn \"$2\" -w $1`\" &&" \
                 "xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
                 p, winid, font, NULL \
         } \
 }
 
-#define MODKEY Mod1Mask
+#define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
 	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
 	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
 
-	{ MODKEY,               XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY,               XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_h,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_l,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_space,  rotate,      { .i = 0 } },
+	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_Escape, spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
@@ -69,8 +69,8 @@ static Key keys[] = {
 
 	{ MODKEY|ShiftMask,     XK_q,      killclient,  { 0 } },
 
-	{ MODKEY,               XK_x,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_x,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY,               XK_u,      focusurgent, { 0 } },
+	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 };
